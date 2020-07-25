f742b9d8d5b09480d2d46ab397a65ce4dbe821a1
Juancho92 <42828299+Juancho92@users.noreply.github.com>
2020-07-25 02:53:31 -0500

    2:53am-25/07/2020

--- src/app/components/sections/toolbar/toolbar.component.ts
+++ src/app/components/sections/toolbar/toolbar.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ToolbarComponent implements OnInit {
   displayLogin:boolean=false;
   displayProfile:boolean = false;
+  noRegisteredClient:boolean = false;
 
   @Input() disableFilter: boolean;
 
