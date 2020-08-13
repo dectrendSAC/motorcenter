c2dcb9d3d8243a6cb78b0bf0f64cd7caba24275e
Juancho92 <42828299+Juancho92@users.noreply.github.com>
2020-08-13 01:20:15 -0500

    1:20am-13/08/2020

---
+++ docs/assets/js/ranWord.js
@@ -0,0 +1,6 @@
+var words = ['APASIONADOS', 'DILIGENTES', 'RESPONSABLES', 'CUIDADOSOS', 'ATENTOS', 'DETALLISTAS'], intervalWords;
+
+function randomWord(){
+        var choosen = words[Math.floor(Math.random()*words.length)]
+        return choosen
+}
