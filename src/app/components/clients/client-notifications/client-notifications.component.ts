12197b140dfabfa8815330214af89baa8277069e
Juancho92 <42828299+Juancho92@users.noreply.github.com>
2020-09-14 01:41:08 -0500

    1:40am-14/09/2020

--- src/app/components/clients/client-notifications/client-notifications.component.ts
+++ src/app/components/clients/client-notifications/client-notifications.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-client-notifications',
   templateUrl: './client-notifications.component.html',
-  styleUrls: ['./client-notifications.component.scss']
+  styleUrls: ['./client-notifications.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ClientNotificationsComponent implements OnInit {
 
