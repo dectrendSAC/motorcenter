5dda0f36a84d700eb13f40d8d6f2c4e35ac045ad
Juancho92 <jscalderon@dectrend.com.pe>
2020-09-15 13:07:19 -0500

    1:07pm-15/09/2020

--- src/app/components/clients/client-notifications/client-notifications.component.ts
+++ src/app/components/clients/client-notifications/client-notifications.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-client-notifications',
@@ -7,10 +8,36 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class ClientNotificationsComponent implements OnInit {
+  allchecked = [];
+
+  clientNotifications = [
+    {tittle: 'Ya puede recoger su vehículo de placa 456-YT8', content:'Se realizaron los trabajos de planchado y pintura por un costo de S/580', date:'20/05/2020 18:43', status:'read', favorite:true},
+    {tittle: 'Ya puede recoger su vehículo de placa 456-YT8', content:'Se realizaron los trabajos de planchado y pintura por un costo de S/580', date:'20/05/2020 18:43', status:'unread', favorite:true},
+    {tittle: 'Ya puede recoger su vehículo de placa 456-YT8', content:'Se realizaron los trabajos de planchado y pintura por un costo de S/580', date:'20/05/2020 18:43', status:'unread', favorite:false}
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  //select all checkboxes method
+  toggleAll(event: MatCheckboxChange) {
+    if ( event.checked ) {
+       this.clientNotifications.forEach(row => {
+          this.allchecked.push(row)
+          });
+    } else {
+       this.allchecked.length = 0 ;
+    }
+  }
+
+  exists(item:any) {
+    return this.allchecked.indexOf(item) > -1;
+  };
+
+  isChecked() {
+    return this.allchecked.length === this.clientNotifications.length;
+  };
 
 }
