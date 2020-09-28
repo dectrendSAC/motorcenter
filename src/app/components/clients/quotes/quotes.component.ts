29d6767224e4b09a0d42eb186d82e809d79c3906
Juancho92 <jscalderon@dectrend.com.pe>
2020-09-28 13:03:57 -0500

    1:03pm - 28/09/2020

--- src/app/components/clients/quotes/quotes.component.ts
+++ src/app/components/clients/quotes/quotes.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class QuotesComponent implements OnInit {
 
   clientQuotes = [
-    {vehicleName: 'Hyundai Atos', vehicleVersion:'sedan', vehicleColorCode:'#212121', vehicleColorName:'Negro', vehiclePrice:50000, vehicleInitialPrice:50000, executive: 'asdasdasd' },
+    {vehicleName: 'Hyundai Atos', vehicleVersion:'sedan', vehicleColorCode:'#212121', vehicleColorName:'Negro', vehiclePrice:50000, vehicleInitialPrice:50000, executive: 'asdasdasd', state: [{date:'2020-02-12T12:47:55Z', description:'Compra del vehículo', status:true}, {date:'2020-02-12T12:47:55Z', description:'Compra del vehículo', status:true}] },
     {vehicleName: 'Hyundai Atos', vehicleVersion:'sedan', vehicleColorCode:'#212121', vehicleColorName:'Negro', vehiclePrice:50000, vehicleInitialPrice:50000, executive: 'asdasdasd' },
     {vehicleName: 'Hyundai Atos', vehicleVersion:'sedan', vehicleColorCode:'#212121', vehicleColorName:'Negro', vehiclePrice:50000, vehicleInitialPrice:50000, executive: 'asdasdasd' }
   ];
