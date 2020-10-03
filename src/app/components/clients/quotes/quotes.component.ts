3f1f571d6a8af5d0582aec0d5667cf7695ae6406
Juancho92 <42828299+Juancho92@users.noreply.github.com>
2020-10-03 01:42:06 -0500

    03/10/2020-1:42am

--- src/app/components/clients/quotes/quotes.component.ts
+++ src/app/components/clients/quotes/quotes.component.ts
@@ -25,7 +25,7 @@ export class QuotesComponent implements OnInit {
   enableReadonly: any;
 
   clientQuotes = [
-    {vehicleName: 'Hyundai Atos', vehicleVersion:'sedan', vehicleColorCode:'#212121', vehicleColorName:'Negro', vehiclePrice:50000, vehicleInitialPrice:50000, executive: 'asdasdasd', state: [{date:'2020-02-12T12:47:55Z', description:'Compra del vehículo', status:true}, {date:'2020-02-12T12:47:55Z', description:'Compra del vehículo', status:true}] },
+    {vehicleName: 'Hyundai Atos', vehicleVersions: [{description:'basico', selected:false}, {description:'full', selected:true}], vehicleColorCode:'#212121', vehicleColorName:'Negro', vehiclePrice:50000, vehicleInitialPrice:50000, executive: 'asdasdasd', state: [{date:'2020-02-12T12:47:55Z', description:'Compra del vehículo', status:true}, {date:'2020-02-12T12:47:55Z', description:'Compra del vehículo', status:true}] },
     {vehicleName: 'Hyundai Atos', vehicleVersion:'sedan', vehicleColorCode:'#212121', vehicleColorName:'Negro', vehiclePrice:50000, vehicleInitialPrice:50000, executive: 'asdasdasd' },
     {vehicleName: 'Hyundai Atos', vehicleVersion:'sedan', vehicleColorCode:'#212121', vehicleColorName:'Negro', vehiclePrice:50000, vehicleInitialPrice:50000, executive: 'asdasdasd' }
   ];
@@ -67,7 +67,7 @@ export class QuotesComponent implements OnInit {
     Object.keys(this.clientQuotes).forEach((i) => {
       this.controlArray.push(
         this._formBuilder.group({
-          kmFormControl: [{value: this.clientQuotes[i].km, disabled: false}, [Validators.required]],
+          versionFormControl: [{value: 'default', disabled: true}, [Validators.required]],
           colorFormControl: [{value: this.clientQuotes[i].colorName, disabled: false}, [Validators.required]]
         })
       )
