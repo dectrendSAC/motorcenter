27bbb990ac6641be49134678e4be39effca4e033
Juancho92 <42828299+Juancho92@users.noreply.github.com>
2020-11-22 02:04:41 -0500

    2:04am - 22/11/2020

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-clients',
@@ -7,15 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientsComponent implements OnInit {
   changeItemDescriptionStatus:boolean;
+  showDistinctTopImage: boolean = false;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    //Check if workshop screen is visible
+    if (this.router.url.indexOf('/taller') > -1) {
+      this.showDistinctTopImage = true;
+    } else {
+      this.showDistinctTopImage = false;
+    }
   }
 
   changeItemDescription(status:boolean){
     this.changeItemDescriptionStatus = status;
     setTimeout(() => { this.changeItemDescriptionStatus = !status }, 100);
+
+    if (document.getElementById('workshopBtn').classList.contains('clicked')) {
+      this.showDistinctTopImage = true;
+    } else {
+      this.showDistinctTopImage = false;
+    }
   }
 
 }
