335221b2a69d9de7a75c5c241557c3ace3cbd2b1
Juancho92 <jscalderon@dectrend.com.pe>
2021-01-28 13:04:06 -0500

    1:03pm - 28/01/2021

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { multipleAnimations } from '../../animations';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +13,7 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   displaySignin:boolean;
+  loginForm: FormGroup;
 
   @Input() noRegisteredClient: boolean;
 
@@ -21,25 +23,37 @@ export class LoginComponent implements OnInit {
   @Output() displayProfile = new EventEmitter<boolean>();
   @Output() displaySuccess = new EventEmitter<boolean>();
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     if (this.router.url.indexOf('/iniciar-sesion') > -1) {
       this.router.navigate([''], {state: {data: true}});
     }
+
+    this.loginForm = this.fb.group({
+      username: [],
+      password: []
+    });
   }
 
   //Hide Login component
-  hideLogin(){    
+  hideLogin(){
     this.displayLogin.emit(false);
     if (this.noRegisteredClient){
       this.changeToolbarClassFromLogin.emit(true);
       this.displayRegister.emit(true);
-    } 
+    }
   }
 
   //Log in client
   logIn(){
+    //Tempo sign in until db works
+    if (this.loginForm.controls['username'].value == 'cliente'){
+      console.log('cliente');
+    } else {
+      console.log('empleado');
+    }
+
     this.displayLogin.emit(false);
     this.displayProfile.emit(true);
     this.changeToolbarClassFromLogin.emit(false);
@@ -47,6 +61,9 @@ export class LoginComponent implements OnInit {
     if(document.getElementsByClassName('register')[0]){
       this.displaySuccess.emit(true);
     }
+  }
+
+  onSubmit() {
   }
 
 
